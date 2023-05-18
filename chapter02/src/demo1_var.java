3724303fe42c24ec782000a9a2191a1c4f5418ff
zbx <3303782144@qq.com>
2023-05-18 17:01:18 +0800

    2 day

--- chapter02/src/demo1_var.java
+++ chapter02/src/demo1_var.java
@@ -1,6 +1,8 @@
 public class demo1_var {
     public static void main(String[] args) {
         int a = 100;
+        int b = 200;
         System.out.println(a);
+        System.out.println(b);
     }
 }
