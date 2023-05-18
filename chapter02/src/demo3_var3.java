2013693c405269239a1d8cef8a06ccf7a04b3be9
zbx <3303782144@qq.com>
2023-05-18 19:11:51 +0800

    2 day

--- chapter02/src/demo3_var3.java
+++ chapter02/src/demo3_var3.java
@@ -2,13 +2,12 @@ import java.lang.reflect.Parameter;
 
 public class demo3_var3 {
     public static void main(String[] args) {
-
+        //5.md
         int a = 50;
         int b = 20;
         String s = "hello";
 
         System.out.println(a + b);
         System.out.println(a + s);
-
     }
 }
---
+++ chapter02/src/demo4_Detail.java
@@ -0,0 +1,17 @@
+public class demo4_Detail {
+    public static void main(String[] args) {
+        //long 数据类型要在后面加L
+        byte b = 1;
+        short s = 2;
+        int i = 3;
+        long l = 4L;
+
+        //float数据类型要在后面加F
+        //浮点数 = 符号位 + 指数位 + 尾数位
+        //尾数位可能会丢失
+        float f = 3.14F;
+        double d = 9.99;
+
+
+    }
+}
