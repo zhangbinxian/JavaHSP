c7bfe96d152365b8acf30771a8f8de36c47d47d7
zbx <3303782144@qq.com>
2023-05-18 18:31:33 +0800

    2 day

--- chapter02/src/demo3_var3.java
+++ chapter02/src/demo3_var3.java
@@ -2,9 +2,13 @@ import java.lang.reflect.Parameter;
 
 public class demo3_var3 {
     public static void main(String[] args) {
-        //细节
-        int a = 50;
 
+        int a = 50;
+        int b = 20;
+        String s = "hello";
+
+        System.out.println(a + b);
+        System.out.println(a + s);
 
     }
 }
