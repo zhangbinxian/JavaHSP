5f097f5d3c6efa79630605030f8285f6e1546c37
zbx <3303782144@qq.com>
2023-05-20 17:28:47 +0800

    4 day

---
+++ chapter03/src/demo02_Scanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class demo02_Scanner {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("请输入name,age,和score: ");
+        String name = sc.next();
+        int age = sc.nextInt();
+        double score = sc.nextDouble();
+
+        System.out.println("name=" + name + " age=" + age + " score=" + score);
+    }
+}
+
