9e5484fe95ec2d3a86da24ca1ad024a1ad5b7de5
zbx <3303782144@qq.com>
2023-05-22 00:19:56 +0800

    6 day

--- chapter04/src/demo03_switch.java
+++ chapter04/src/demo03_switch.java
@@ -2,7 +2,37 @@ import java.util.Scanner;
 
 public class demo03_switch {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入一个字符:");
+        //sc.next()是String的方法
+        char weekdays = sc.next().charAt(0);
 
+        switch (weekdays) {
+            case 'a':
+                System.out.println("星期一");
+                break;
+            case 'b':
+                System.out.println("星期二");
+                break;
+            case 'c':
+                System.out.println("星期四");
+                break;
+            case 'd':
+                System.out.println("星期四");
+                break;
+            case 'e':
+                System.out.println("星期五");
+                break;
+            case 'f':
+                System.out.println("星期六");
+                break;
+            case 'g':
+                System.out.println("星期天");
+                break;
+            default:
+                System.out.println("error");
+                break;
 
+        }
     }
 }
