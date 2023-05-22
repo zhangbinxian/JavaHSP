74b7826e3e68ed76a1bd2cbe2ffffd1505fcefd0
zbx <3303782144@qq.com>
2023-05-22 23:00:27 +0800

    6 day

---
+++ chapter04/src/demo06_while.java
@@ -0,0 +1,10 @@
+public class demo06_while {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("while结束,i=" + i);
+    }
+}
