7bc26190dda5645464984f29268a76d0cf4e0423
zbx <3303782144@qq.com>
2023-05-23 00:21:13 +0800

    7 day

---
+++ chapter04/src/test.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            for (int k = 0; k < n - i; k++) {
+                System.out.print(" ");
+            }
+            System.out.print("-");
+            if (i == n - 1) {
+                for (int j = 0; j < 2 * (i - 1) + 1; j++) {
+                    System.out.print("-");
+                }
+            } else {
+                for (int j = 0; j < 2 * (i - 1) + 1; j++) {
+                    System.out.print(" ");
+                }
+            }
+            if (i != 0) {
+                System.out.print("-");
+            }
+            System.out.println();
+
+
+        }
+    }
+}
+
