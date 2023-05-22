5b9a070b09d0955b8c7e04f0c7a445d14cbcb5d2
zbx <3303782144@qq.com>
2023-05-22 23:00:08 +0800

    6 day

--- chapter04/src/demo05_for.java
+++ chapter04/src/demo05_for.java
@@ -1,6 +1,6 @@
 public class demo05_for {
     public static void main(String[] args) {
-        for (int i = 0; i <10; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("当前是" + i);
         }
     }
---
+++ chapter04/src/demo07_dowhile.java
@@ -0,0 +1,10 @@
+public class demo07_dowhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 3);
+        System.out.println("do while结束,i=" + i);
+    }
+}
