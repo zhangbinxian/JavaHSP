9f360ccce11e86ee9e0d73a0fd227b66d8eed27e
zbx <3303782144@qq.com>
2023-05-24 15:23:20 +0800

    8 day 23/5/24

---
+++ chapter05/src/demo01_Array.java
@@ -0,0 +1,12 @@
+public class demo01_Array {
+    public static void main(String[] args) {
+        int []a=new int[3];
+        for (int i = 0; i < a.length; i++) {
+            a[i]=i;
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+}
