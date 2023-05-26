1bd740d33c0a9ee92ac8dfc6268c88c9b52c88d3
zbx <3303782144@qq.com>
2023-05-27 00:33:02 +0800

    10 day 23/5/26

--- chapter06/src/demo03.java
+++ chapter06/src/demo03.java
@@ -1,11 +1,41 @@
 public class demo03 {
     public static void main(String[] args) {
+        People_demo03 p1 = new People_demo03();
+        p1.name = "a";
+        p1.age = 10;
+        p1.speak();
+        p1.calc();
+        p1.calcN(100);
+        p1.getSum(12, 100);
 
     }
 }
-class People_demo03{
+
+class People_demo03 {
     String name;
     int age;
 
+    public void speak() {
+        System.out.println("i am " + this.name);
+    }
 
+    public void calc() {
+        int sum = 0;
+        for (int i = 0; i <= 1000; i++) {
+            sum += i;
+        }
+        System.out.println("0+...+100=" + sum);
+    }
+
+    public void calcN(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("0+...+" + n + "=" + sum);
+    }
+
+    public void getSum(int num1, int num2) {
+        System.out.println(num1 + "+" + num2 + "=" + (num1 + num2));
+    }
 }
