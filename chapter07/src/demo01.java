0272f6dac824025fe8a94b12ace2daade41ba3fb
zbx <3303782144@qq.com>
2023-07-14 17:10:03 +0800

    13 day 23/7/14

--- chapter07/src/demo01.java
+++ chapter07/src/demo01.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class demo01 {
     public static void main(String[] args) {
-        System.out.println("as");
+        int[] arr = {-1, 2, 4, 1, 5, -10, 22};
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
