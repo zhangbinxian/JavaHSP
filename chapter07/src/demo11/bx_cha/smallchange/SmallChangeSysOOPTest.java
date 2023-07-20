6192b67753c3f763a19f42e126ebfd649514b49b
zbx <3303782144@qq.com>
2023-07-20 17:53:42 +0800

    23/7/20

--- chapter07/src/demo11/bx_cha/smallchange/SmallChangeSysOOPTest.java
+++ chapter07/src/demo11/bx_cha/smallchange/SmallChangeSysOOPTest.java
@@ -2,6 +2,7 @@ package demo11.bx_cha.smallchange;
 
 public class SmallChangeSysOOPTest {
     public static void main(String[] args) {
+
         SmallChangeSysOOP smallChangeSysOOP = new SmallChangeSysOOP();
         smallChangeSysOOP.showMenu();
     }
